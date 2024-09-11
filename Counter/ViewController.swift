564fe64856977eea90831e5cab60774061dcd96b
Victoria <syatkina-16@yandex.ru>
2024-09-11 22:36:18 +0300

    Выполнина базовая задача спринта 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterValueLabel: UILabel!
+    
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    private var counterValue: String?
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        if let currentCounterValue = Int(counterValue ?? "0") {
+        
+            let newCounterValue = currentCounterValue + 1
+            
+            counterValue = String(newCounterValue)
+        } else {
+            
+            counterValue = "0"
+        }
+        
+        counterValueLabel.text = counterValue
+    }
+    
+   
 
 }
 
