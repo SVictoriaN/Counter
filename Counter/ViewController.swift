55022208531b9af81fae44ae976ebd164d9dec22
SVictoriaN <syatkina-16@yandex.ru>
2026-06-17 21:12:54 +0300

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Виктория Сяткина on 17.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
